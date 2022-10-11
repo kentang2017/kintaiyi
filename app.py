e94b0d725cf9da279787aa333b90259391b4556a
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2022-10-11 11:51:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -77,9 +77,11 @@ with st_capture(output5.code):
             st.image(open("kook/"+yy+str(ttext.get("局式").get("數"))+".svg").read())
             st.markdown("《太乙秘書》︰")
             st.markdown(ts)
+            st.markdown("史事記載︰")
+            st.markdown(cys)
         except (FileNotFoundError,IndexError):
             st.empty()
-        print("{} |\n{} |\n{} |\n太乙{} - {} | 積年數︰{} | \n紀元︰{} | \n\n史事記載︰\n {} \n\n《太乙秘書》︰\n{}".format(ttext.get("公元日期"), gz, ttext.get("年號"), ttext.get("太乙計"),  ttext.get("局式").get("文"), ty.accnum(num), ttext.get("紀元"), cys, tys))
+        print("{} |\n{} |\n{} |\n太乙{} - {} | 積年數︰{} | \n紀元︰{} | \n\n".format(ttext.get("公元日期"), gz, ttext.get("年號"), ttext.get("太乙計"),  ttext.get("局式").get("文"), ty.accnum(num), ttext.get("紀元")))
         expander = st.expander("原始碼")
         expander.write(str(ttext))
     else:
