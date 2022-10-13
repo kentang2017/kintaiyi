46e7bb3a5b8f739f738563e4d96b8583ecbc919d
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2022-10-13 15:56:00 +0800

    Update kintaiyi.py

--- kintaiyi.py
+++ kintaiyi.py
@@ -407,7 +407,7 @@ class Taiyi():
                     "癸":{"朝":"太乙", "暮":"太衝"}, "壬":{"朝":"太衝", "暮":"太乙"}, 
                     "辛":{"朝":"功曹", "暮":"勝光"}}
             general = "天乙,螣蛇,朱雀,六合,勾陳,青龍,天空,白虎,太常,玄武,太陰,天后".split(",")
-            tianyi = ztz.get(self.multi_key_dict_get(gzzm, self.gangzhi()[2]).get( self.multi_key_dict_get(zm, self.gangzhi()[3]) ))
+            tianyi = ztz.get(self.multi_key_dict_get(gzzm, self.gangzhi()[2][0]).get( self.multi_key_dict_get(zm, self.gangzhi()[3][1]) ))
             return dict(zip(self.new_list(self.Zhi,tianyi),general))
         else:
             return "None"
