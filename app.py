e2e41a35f8c7b8c5b14f2287cbc17fd0d5536bf3
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2022-10-19 17:47:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -116,7 +116,7 @@ with st_capture(output5.code):
             else:
                 yy = "yin"
             try:
-                components.html('''<html><body><object data="tykook/typan.svg"/></body></html> ''')
+                components.html('''<html><body><object data="tykook/typan.svg" type="image/svg+xml" width="200" height="200" id="mySVGObject"></body></html> ''')
                 st.image(open("kook/"+yy+str(ttext.get("局式").get("數"))+".svg").read(), use_column_width=True)
                 st.title("《太乙秘書》︰")
                 st.markdown(ts)
