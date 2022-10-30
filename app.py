5e9e6fbd84e7dce5314055c5dfd755427a2d6699
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2022-10-30 10:34:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,6 @@ def st_capture(output_func):
         yield
         
 st.set_page_config(layout="wide",page_title="太鳦 - 太乙排盘")
-st.header('太鳦')
 pan,example,guji = st.tabs([' 排盤 ', ' 案例 ', ' 古籍 ' ])
 
 with pan:
