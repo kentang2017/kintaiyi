bab6482bfe949d94d5592aeeec7e01a1656d7c15
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2022-10-30 22:23:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -53,51 +53,54 @@ with pan:
     with st_capture(output5.code):
         col1, col2 = st.columns(2)
         with col1:
-            if st.button('手動'):
-                p = str(idate).split("/")
-                pp = str(itime).replace("：",":").split(":")
-                y = int(p[0])
-                m = int(p[1])
-                d = int(p[2])
-                h = int(pp[0])
-                min = int(pp[1])
-                ty = Taiyi(y,m,d,h,min)
-                ttext = Taiyi(y,m,d,h,min).pan(num)
-                kook = Taiyi(y,m,d,h,min).kook(num)
-                ts = taiyi_yingyang.get(kook.get('文')[0:2]).get(kook.get('數'))
-                gz = "{}年 {}月 {}日 {}時".format(ttext.get("干支")[0], ttext.get("干支")[1], ttext.get("干支")[2], ttext.get("干支")[3])
-                ch = chistory.get(y)
-                r = list(map(lambda x:[x, x+25]  ,list(range(0,3000)[0::25])))
-                tys = "".join([ts[r[i][0]:r[i][1]]+"\n" for i in range(0, int(len(ts) / 25+1))])
-                #try:
-                    #cys = "".join([ch[r[i][0]:r[i][1]]+"\n" for i in range(0, int(len(ch) / 25+1))])
-                #except (TypeError,IndexError):
-                    #cys = ""
-                if ttext.get("局式").get("文")[0] == "陽":
-                    yy = "yang"
+            try:
+                if st.button('手動'):
+                    p = str(idate).split("/")
+                    pp = str(itime).replace("：",":").split(":")
+                    y = int(p[0])
+                    m = int(p[1])
+                    d = int(p[2])
+                    h = int(pp[0])
+                    min = int(pp[1])
+                    ty = Taiyi(y,m,d,h,min)
+                    ttext = Taiyi(y,m,d,h,min).pan(num)
+                    kook = Taiyi(y,m,d,h,min).kook(num)
+                    ts = taiyi_yingyang.get(kook.get('文')[0:2]).get(kook.get('數'))
+                    gz = "{}年 {}月 {}日 {}時".format(ttext.get("干支")[0], ttext.get("干支")[1], ttext.get("干支")[2], ttext.get("干支")[3])
+                    ch = chistory.get(y)
+                    r = list(map(lambda x:[x, x+25]  ,list(range(0,3000)[0::25])))
+                    tys = "".join([ts[r[i][0]:r[i][1]]+"\n" for i in range(0, int(len(ts) / 25+1))])
+                    #try:
+                        #cys = "".join([ch[r[i][0]:r[i][1]]+"\n" for i in range(0, int(len(ch) / 25+1))])
+                    #except (TypeError,IndexError):
+                        #cys = ""
+                    if ttext.get("局式").get("文")[0] == "陽":
+                        yy = "yang"
+                    else:
+                        yy = "yin"
+                    try:
+                        st.image(open("kook/"+yy+str(ttext.get("局式").get("數"))+".svg").read(), use_column_width=True)
+                        st.title("《太乙秘書》︰")
+                        st.markdown(ts)
+                        st.title("史事記載︰")
+                        st.markdown(ch)
+                        st.title("九宮分野︰")
+                        st.image("pic/太乙九宮分野圖.jpg", use_column_width=True)
+                    except (FileNotFoundError,IndexError, ValueError):
+                        st.empty()
+                        st.title("《太乙秘書》︰")
+                        st.markdown(ts)
+                        st.title("史事記載︰")
+                        st.markdown(ch)
+                        st.title("九宮分野︰")
+                        st.image("pic/太乙九宮分野圖.jpg", use_column_width=True)
+                    print("{} |\n{} |\n{} |\n太乙{} - {} | 積年數︰{} | \n紀元︰{} | \n\n".format(ttext.get("公元日期"), gz, ttext.get("年號"), ttext.get("太乙計"),  ttext.get("局式").get("文"), ty.accnum(num), ttext.get("紀元")))
+                    expander = st.expander("原始碼")
+                    expander.write(str(ttext))
                 else:
-                    yy = "yin"
-                try:
-                    st.image(open("kook/"+yy+str(ttext.get("局式").get("數"))+".svg").read(), use_column_width=True)
-                    st.title("《太乙秘書》︰")
-                    st.markdown(ts)
-                    st.title("史事記載︰")
-                    st.markdown(ch)
-                    st.title("九宮分野︰")
-                    st.image("pic/太乙九宮分野圖.jpg", use_column_width=True)
-                except (FileNotFoundError,IndexError, ValueError):
                     st.empty()
-                    st.title("《太乙秘書》︰")
-                    st.markdown(ts)
-                    st.title("史事記載︰")
-                    st.markdown(ch)
-                    st.title("九宮分野︰")
-                    st.image("pic/太乙九宮分野圖.jpg", use_column_width=True)
-                print("{} |\n{} |\n{} |\n太乙{} - {} | 積年數︰{} | \n紀元︰{} | \n\n".format(ttext.get("公元日期"), gz, ttext.get("年號"), ttext.get("太乙計"),  ttext.get("局式").get("文"), ty.accnum(num), ttext.get("紀元")))
-                expander = st.expander("原始碼")
-                expander.write(str(ttext))
-            else:
-                st.empty()
+            except ValueError:
+                 st.empty()
         with col2:
              if st.button('即時'):
                 now = datetime.datetime.now(pytz.timezone('Asia/Hong_Kong'))
