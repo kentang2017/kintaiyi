20f177348f775c95da0961c3729b73c6e3a4f87f
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2022-11-05 22:05:23 +0800

    Update kintaiyi.py

--- kintaiyi.py
+++ kintaiyi.py
@@ -78,7 +78,7 @@ class Taiyi():
                 cyear = "元"
             else:
                 cyear = an2cn(year)
-            pn = "{}{}年".format(preiodname[idx-1],cyear)
+            pn = "{}{}年".format(preiodname[idx],cyear)
             kn = "{}{}{}".format(period[idx], king[idx], king_realname[idx])
             return  "{} {}".format(kn, pn)
         elif year >= 1900:
