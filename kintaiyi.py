e9f8820ea6750116d8b81d6d13d7de00dcede894
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2022-11-05 15:33:03 +0800

    Update kintaiyi.py

--- kintaiyi.py
+++ kintaiyi.py
@@ -73,7 +73,7 @@ class Taiyi():
         idx = y.index(closest(y, self.lunar_date_d().get("年")))
         year = year = self.lunar_date_d().get("年")
         if year < 1900:
-            year = year - y[idx] +1 
+            year = y[idx] +1 - year
             if year < 0:
                 year =  y[idx-1] - year
                 cyear = an2cn(year)
