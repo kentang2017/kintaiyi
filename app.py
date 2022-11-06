f9f8bad2b638a784ea99ebdbebec601adab85fd8
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2022-11-06 23:56:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def st_capture(output_func):
         yield
         
 st.set_page_config(layout="wide",page_title="太鳦 - 太乙排盘")
-pan,example,guji,update = st.tabs([' 排盤 ', ' 史例 ', ' 古籍 ',' 日誌 ' ])
+pan,example,guji,update,instruction = st.tabs([' 排盤 ', ' 史例 ', ' 古籍 ',' 日誌 ', ' 使用說明 ' ])
 
 with st.sidebar:
     idate = st.text_input('輸入日期(如: 1997/8/8)', '')
@@ -267,3 +267,7 @@ with guji:
 with update:
     st.header('更新日誌')
     st.markdown(get_file_content_as_string("update.md"))
+
+with instruction:
+    st.header('使用說明')
+    st.markdown(get_file_content_as_string("instruction.md"))
