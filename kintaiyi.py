8261a07552ec0c73a4f4cd7481fd83555ef5bbc3
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2022-11-07 21:01:04 +0800

    Update kintaiyi.py

--- kintaiyi.py
+++ kintaiyi.py
@@ -251,12 +251,12 @@ class Taiyi():
             return int(Date("{}/{}/{} {}:00:00.00".format(str(self.year).zfill(4), str(self.month).zfill(2), str(self.day).zfill(2), str(self.hour).zfill(2))) - Date("1900/06/19 00:00:00.00")) 
             #return (datetime.strptime("{0:04}-{1:02d}-{2:02d} 00:00:00".format(self.year, self.month, self.day), "%Y-%m-%d %H:%M:%S") - datetime.strptime("1900-06-19 00:00:00","%Y-%m-%d %H:%M:%S")).days
         elif ji == 3: #時計
-            dgz = self.gangzhi()[2]
-            hz =  self.gangzhi()[3][1]
-            a = "甲子甲午庚子庚午丙子丙午壬子壬午戊子戊午,乙丑乙未辛丑辛未丁丑丁未癸未癸丑己丑己未,丙寅丙申壬寅壬申戊寅戊申甲寅甲申庚寅庚申,丁卯丁酉癸卯癸酉己卯己酉乙卯乙酉辛卯辛酉,戊辰戊戌甲戌甲辰庚辰庚戌丙戌丙辰壬辰壬戌,己巳己亥乙亥乙已辛已辛亥丁亥丁巳癸已癸亥".split(",")
-            b = dict(zip([tuple(re.findall('..', i)) for i in a], [1,13,25,37,49,61]))
-            num  = self.multi_key_dict_get(b, dgz)
-            return dict(zip(self.Zhi, range(num, num + 12))).get(hz)
+            #dgz = self.gangzhi()[2]
+            #hz =  self.gangzhi()[3][1]
+            #a = "甲子甲午庚子庚午丙子丙午壬子壬午戊子戊午,乙丑乙未辛丑辛未丁丑丁未癸未癸丑己丑己未,丙寅丙申壬寅壬申戊寅戊申甲寅甲申庚寅庚申,丁卯丁酉癸卯癸酉己卯己酉乙卯乙酉辛卯辛酉,戊辰戊戌甲戌甲辰庚辰庚戌丙戌丙辰壬辰壬戌,己巳己亥乙亥乙已辛已辛亥丁亥丁巳癸已癸亥".split(",")
+            #b = dict(zip([tuple(re.findall('..', i)) for i in a], [1,13,25,37,49,61]))
+            #num  = self.multi_key_dict_get(b, dgz)
+            #return dict(zip(self.Zhi, range(num, num + 12))).get(hz)
             #jiazi_ac = (tn_num + self.year) * 365.2425 * 12 -1
             #zhi_code = dict(zip(self.Zhi, range(1,13)))
             #hz = self.gangzhi()[3][1]
@@ -265,10 +265,10 @@ class Taiyi():
             #    return int(acc)
             #else:
             #    return int(jiazi_ac)
-            #if self.hour == 0:
-            #    return (int((Date("{}/{}/{} {}:00:00.00".format(str(self.year).zfill(4), str(self.month).zfill(2), str(self.day).zfill(2), str(self.hour).zfill(2))) - Date("1900/06/19 00:00:00.00") - 1)) * 12 + (self.hour + 1 ) // 2 + 1) + 12
-            #else:
-            #    return int((Date("{}/{}/{} {}:00:00.00".format(str(self.year).zfill(4), str(self.month).zfill(2), str(self.day).zfill(2), str(self.hour).zfill(2))) - Date("1900/06/19 00:00:00.00") - 1)) * 12 + (self.hour + 1 ) // 2 + 1 
+            if self.hour == 0:
+                return (int((Date("{}/{}/{} {}:00:00.00".format(str(self.year).zfill(4), str(self.month).zfill(2), str(self.day).zfill(2), str(self.hour).zfill(2))) - Date("1900/06/19 00:00:00.00") - 1)) * 12 + (self.hour + 1 ) // 2 + 1) + 12
+            else:
+                return int((Date("{}/{}/{} {}:00:00.00".format(str(self.year).zfill(4), str(self.month).zfill(2), str(self.day).zfill(2), str(self.hour).zfill(2))) - Date("1900/06/19 00:00:00.00") - 1)) * 12 + (self.hour + 1 ) // 2 + 1 
             #return ((datetime.strptime("{0:04}-{1:02d}-{2:02d} 00:00:00".format(self.year, self.month, self.day), "%Y-%m-%d %H:%M:%S") - datetime.strptime("1900-06-19 00:00:00","%Y-%m-%d %H:%M:%S")).days - 1 ) * 12 + (self.hour + 1 ) // 2 + 1
         elif ji == 4: #分計
             return int((Date("{}/{}/{} {}:00:00.00".format(str(self.year).zfill(4), str(self.month).zfill(2), str(self.day).zfill(2), str(self.hour).zfill(2))) - Date("1900/06/19 00:00:00.00") - 1)) * 120 + (self.minute + 1 ) // 2 + 1 
@@ -291,7 +291,7 @@ class Taiyi():
             else:
                 dun = "陽遁"
             three_year = {0:"理天", 1:"理地", 2:"理人"}.get(dict(zip(list(range(1,73)), [0,1,2] * 24)).get(k))
-            k = self.accnum(ji, tn)
+            k = self.accnum(ji, tn)%72%24
             return {"文":"{}{}局".format(dun, an2cn(k)), "數":k, "年":three_year}
     
     def getyuan(self, ji, tn):
