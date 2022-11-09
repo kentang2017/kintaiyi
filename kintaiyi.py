078464bc24b741c6cefe7332a1c0217984003167
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2022-11-09 23:05:13 +0800

    Update kintaiyi.py

--- kintaiyi.py
+++ kintaiyi.py
@@ -268,7 +268,7 @@ class Taiyi():
             #else:
             #    return int(jiazi_ac)
             t = 708011105
-            accday = t + (int((Date("{}/{}/{} {}:00:00.00".format(str(self.year).zfill(4), str(self.month).zfill(2), str(self.day).zfill(2), str(self.hour).zfill(2))) - Date("1900/12/21 00:00:00.00")))
+            accday = t + int(Date("{}/{}/{} {}:00:00.00".format(str(self.year).zfill(4), str(self.month).zfill(2), str(self.day).zfill(2), str(self.hour).zfill(2)) - Date("1900/12/21 00:00:00.00"))
             acchr = (accday -1)*12+ (self.hour + 1)//2+1
             #if self.hour == 0:
             #    return (int((Date("{}/{}/{} {}:00:00.00".format(str(self.year).zfill(4), str(self.month).zfill(2), str(self.day).zfill(2), str(self.hour).zfill(2))) - Date("1900/06/19 00:00:00.00") - 1)) * 12 + (self.hour + 1 ) // 2 + 1) + 12
