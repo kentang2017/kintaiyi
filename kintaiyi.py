62c5362d30afc6e7ad5801cf5c86d863df075add
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2022-11-17 08:56:22 +0800

    Update kintaiyi.py

--- kintaiyi.py
+++ kintaiyi.py
@@ -274,7 +274,7 @@ class Taiyi():
             elif tn ==2:
                 t = 708011105 - 10153917 + tn_num
             elif tn ==1:
-                t = tn_num 
+                t = tn_num * 365.2425
             accday = t + int((Date("{}/{}/{} {}:00:00.00".format(str(self.year).zfill(4), str(self.month).zfill(2), str(self.day).zfill(2), str(self.hour).zfill(2))) - Date("1900/12/21 00:00:00.00") )) 
             acchr = ((accday -1) * 12) + (self.hour+1)//2 +1
             return acchr
