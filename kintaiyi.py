f2ca8c9f52b323bf7cc8de22d00be5ed4fdb4587
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2023-01-02 23:35:52 +0800

    Update kintaiyi.py

--- kintaiyi.py
+++ kintaiyi.py
@@ -910,7 +910,7 @@ class Taiyi():
             return "主客旗鼓相當。"
     #陽九
     def yangjiu(self):
-        getyj = (self.year + 12607)%4560%456//10
+        getyj = (self.year + 12607)%4560%456//13
         if getyj>=12:
           getyj = getyj % 12
         return dict(zip(range(0,13),self.Zhi)).get(getyj)
