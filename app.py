36cf2bd84002d8eeb00a2fccd4b69db2249bfcec
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2023-04-13 16:42:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def st_capture(output_func):
         
 
 st.set_page_config(layout="wide",page_title="太鳦 - 太乙排盘")
-pan,example,disaster,guji,update,ins = st.tabs([' 排盤 ', ' 史例 ', ' 災異 ' ,' 古籍 ',' 日誌 ', ' 說明 ' ])
+pan,example,disaster,guji,update,ins,tutorial = st.tabs([' 排盤 ', ' 史例 ', ' 災異 ' ,' 古籍 ',' 日誌 ', ' 說明 ', ' 教學 ' ])
 hide_st_style = """
             <style>
             #MainMenu {visibility: hidden;}
