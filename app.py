1e3bd434653f673b62ff0e39a181bf06ff297f09
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2023-05-09 23:35:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -109,7 +109,8 @@ with pan:
                     yy = "yang"
                 else:
                     yy = "yin"
-                st.image("kook/"+yy+str(ttext.get("局式").get("數"))+".svg", use_column_width=True)
+                #st.image("kook/"+yy+str(ttext.get("局式").get("數"))+".svg", use_column_width=True)
+                st.image(open("kook/"+yy+str(ttext.get("局式").get("數"))+".svg").read(), use_column_width=True)
                 st.title("《太乙秘書》︰")
                 st.markdown(ts)
                 st.title("史事記載︰")
@@ -173,7 +174,7 @@ with pan:
                 yy = "yang"
             else:
                 yy = "yin"
-            st.image("kook/"+yy+str(ttext.get("局式").get("數"))+".svg")
+            st.image(open("kook/"+yy+str(ttext.get("局式").get("數"))+".svg").read(), use_column_width=True)
             st.title("《太乙秘書》︰")
             st.markdown(ts)
             st.title("史事記載︰")
