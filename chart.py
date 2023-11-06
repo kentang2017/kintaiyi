90344256f73e2cde5e8ed0a5846541c2c5755e15
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2023-11-06 18:25:31 +0800

    Update chart.py

--- chart.py
+++ chart.py
@@ -21,7 +21,7 @@ def gen_chart(first_layer, second_layer, sixth_layer):
     # Create an SVG drawing canvas
     d = draw.Drawing(380, 420, origin="center")
     # Set the donut's radii and number of divisions for each layer
-    inner_radius = 15
+    inner_radius = 13
     layer_gap = 45  # Gap between layers
     num_divisions = [1, 8, 16, 16]
     # Define the data for each layer
