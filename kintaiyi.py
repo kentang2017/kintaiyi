69986ba8efe387309241e61ea2a93f340e8e29a4
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2023-11-06 09:18:50 +0800

    Update kintaiyi.py

--- kintaiyi.py
+++ kintaiyi.py
@@ -423,7 +423,7 @@ class Taiyi():
                  {config.num2gong(self.away_vgen(ji_style, taiyi_acumyear)):"客參"},
                  {config.num2gong(config.threewind(self.accnum(ji_style,taiyi_acumyear))):"三風"},  
                  {config.num2gong(config.fivewind(self.accnum(ji_style,taiyi_acumyear))):"五風"},
-                 {config.wufu(self.accnum(self.accnum(ji_style,taiyi_acumyear))):"五福"},
+                 {config.wufu(self.accnum(ji_style,taiyi_acumyear)):"五福"},
                  {config.num2gong(config.eightwind(self.accnum(ji_style,taiyi_acumyear))):"八風"},  
                  {config.num2gong(config.flybird(self.accnum(ji_style,taiyi_acumyear))):"飛鳥"},
                  {config.num2gong(config.bigyo(self.accnum(ji_style,taiyi_acumyear))):"大游"},
