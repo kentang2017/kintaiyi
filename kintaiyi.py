f16703e02a3c59bda296e95d64d97cf2cb10197a
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2023-11-07 21:35:04 +0800

    Update kintaiyi.py

--- kintaiyi.py
+++ kintaiyi.py
@@ -266,7 +266,8 @@ class Taiyi():
         if wc_jc ==1 and ty_jc ==1 and wc_jc1 !=1 :
             return sum(wc_order[: wc_order.index(taiyi)])+1
         if wc_jc !=1 and ty_jc ==1 and wc_jc1 ==1 :
-            return wc_order[wc_jc]
+            #return wc_order[wc_jc]
+            return sum(wc_order[: wc_order.index(taiyi)])
         if wc_jc !=1 and ty_jc !=1 and wc_jc1 !=1 and taiyi != wc_num:
             return sum(wc_order[: wc_order.index(taiyi)])
         if wc_jc !=1 and ty_jc !=1 and wc_jc1 !=1 and taiyi == wc_num:
