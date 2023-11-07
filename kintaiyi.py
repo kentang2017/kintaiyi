840d2aee4fb439fd0175016e9a6d4f2aed3021a0
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2023-11-07 12:13:10 +0800

    Update kintaiyi.py

--- kintaiyi.py
+++ kintaiyi.py
@@ -345,7 +345,7 @@ class Taiyi():
     def shensha(self, ji_style, taiyi_acumyear):
         """推太乙當時法"""
         if ji_style ==3:
-            general = "天乙,螣蛇,朱雀,六合,勾陳,青龍,天空,白虎,太常,玄武,太陰,天后".split(",")
+            general = "貴人,螣蛇,朱雀,六合,勾陳,青龍,天空,白虎,太常,玄武,太陰,天后".split(",")
             tiany = self.skyyi(ji_style, taiyi_acumyear).replace("兌", "酉").replace("坎", "子").replace("震","卯").replace("離","午").replace("艮", "丑")
             kook = self.kook(ji_style, taiyi_acumyear).get("文")[0]
             if kook == "陽":
