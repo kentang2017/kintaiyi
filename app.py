abbff54d0429637265d4701bf7dd0b75c181d920
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2023-11-10 00:52:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -32,10 +32,10 @@ def timeline(data, height=800):
     js_block  = f'<script src="{cdn_path}/js/timeline.js"></script>'
     htmlcode = css_block + ''' 
     ''' + js_block + '''
-        <div id='timeline-embed' style="width: 95%; height: '''+str(height)+'''px; margin: 1px;"></div>
+        <div id='timeline-embed' style="width: 95%; height: '''+str(height)+'''px; margin: 1px;  "></div>
         <script type="text/javascript">
             var additionalOptions = {
-                start_at_end: false, is_embed:false, default_bg_color: {r:0, g:0, b:0}
+                start_at_end: false, is_embed:false, default_bg_color: {r:0, g:0, b:0}, color: white
             }
             '''+source_block+'''
             timeline = new TL.Timeline('timeline-embed', '''+source_param+''', additionalOptions);
