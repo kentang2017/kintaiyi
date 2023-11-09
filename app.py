0dc82bfa5ddbf5affa78b8574be571b1128d60d4
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2023-11-09 11:07:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -122,7 +122,7 @@ with pan:
                 if confirmationEdit.is_open():
                     with confirmationEdit.container():
                         st.markdown(""" ### ¿Deseas guardar los cambios? """)
-
+                st.markdown("{} | \n農曆︰{} | {} |\n{} |\n{} |\n{} - {} - {} ({}) |\n積{}數︰{} | \n紀元︰{} | 主筭︰{} 客筭︰{} |\n{}禽值年 | {}門值事 | \n{}卦值年 | 太乙統運卦︰{} | \n ".format(config.gendatetime(my,mm,md,mh), lunard, jieqi.jq(my,mm,md,mh,mmin),gz, config.kingyear(my), config.ty_method(tn), config.taiyi_name(num), ty.kook(num, tn).get("文") ,ttext.get("局式").get("年"),  config.taiyi_name(num)[0],tynum, ttext.get("紀元"), homecal, awaycal, yc, ed, g, config.find_gua(config.lunar_date_d(my, mm, md).get("年"))  ))
                 st.title("《太乙秘書》︰")
                 st.markdown(ts)
                 st.title("史事記載︰")
@@ -135,6 +135,7 @@ with pan:
                 #st.markdown("推猛虎相拒︰"+ttext.get("推猛虎相拒"))
                 #st.markdown("推白龍得雲︰"+ttext.get("推白龍得雲"))
                 #st.markdown("推回軍無言︰"+ttext.get("推回軍無言"))
+                
                 st.markdown("太歲值宿斷事︰"+ year_predict)
                 st.markdown("始擊值宿斷事︰"+ sj_su_predict)
                 st.markdown("十天干歲始擊落宮預測︰"+ tg_sj_su_predict)
@@ -145,7 +146,7 @@ with pan:
                 st.markdown("推太乙風雲飛鳥助戰︰"+ home_vs_away3)
                 #st.title("九宮分野︰")
                 #st.image("pic/太乙九宮分野圖.jpg", use_column_width=True)
-                print("{} | \n農曆︰{} | {} |\n{} |\n{} |\n{} - {} - {} ({}) |\n積{}數︰{} | \n紀元︰{} | 主筭︰{} 客筭︰{} |\n{}禽值年 | {}門值事 | \n{}卦值年 | 太乙統運卦︰{} | \n ".format(config.gendatetime(my,mm,md,mh), lunard, jieqi.jq(my,mm,md,mh,mmin),gz, config.kingyear(my), config.ty_method(tn), config.taiyi_name(num), ty.kook(num, tn).get("文") ,ttext.get("局式").get("年"),  config.taiyi_name(num)[0],tynum, ttext.get("紀元"), homecal, awaycal, yc, ed, g, config.find_gua(config.lunar_date_d(my, mm, md).get("年"))  ))
+                #print("{} | \n農曆︰{} | {} |\n{} |\n{} |\n{} - {} - {} ({}) |\n積{}數︰{} | \n紀元︰{} | 主筭︰{} 客筭︰{} |\n{}禽值年 | {}門值事 | \n{}卦值年 | 太乙統運卦︰{} | \n ".format(config.gendatetime(my,mm,md,mh), lunard, jieqi.jq(my,mm,md,mh,mmin),gz, config.kingyear(my), config.ty_method(tn), config.taiyi_name(num), ty.kook(num, tn).get("文") ,ttext.get("局式").get("年"),  config.taiyi_name(num)[0],tynum, ttext.get("紀元"), homecal, awaycal, yc, ed, g, config.find_gua(config.lunar_date_d(my, mm, md).get("年"))  ))
             else:
                 output5 = st.empty()
         except ValueError:
