9cb7110bcabf6b9091f8d1e2ad4bd2810edc03fe
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2023-12-14 19:18:09 +0800

    Update kintaiyi.py

--- kintaiyi.py
+++ kintaiyi.py
@@ -569,7 +569,7 @@ class Taiyi():
             return chart.gen_chart_hour( list(self.sixteen_gong( ji_style, taiyi_acumyear).values())[-1], self.geteightdoors_text2(ji_style, taiyi_acumyear), sg,list(self.sixteen_gong( ji_style, taiyi_acumyear).values())[:-1])
 
     def gen_life_gong(self, sex):
-        res = {"巳":" ", "午":" ", "未":" ", "坤":" ", "申":" ", "酉":" ", "戌":" ", "乾":" ", "亥":" ", "子":" ", "丑":" ", "艮":" ","寅":" ", "卯":" ", "辰":" ", "巽":" "}
+        res = {"巳":" ", "午":" ", "未":" ", "申":" ", "酉":" ", "戌":" ", "亥":" ", "子":" ", "丑":" ","寅":" ", "卯":" ", "辰":" "}
         dict1 = self.taiyi_life(sex).get("十二命宮排列")
         res.update(dict1)
         sg = list(res.values())
