92b5934e5433e3c5456b3dc68e7af4226edd7b6f
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2023-12-15 10:52:14 +0800

    Update kintaiyi.py

--- kintaiyi.py
+++ kintaiyi.py
@@ -583,7 +583,7 @@ class Taiyi():
         sg = list(res.values())
         return  list(self.sixteen_gong1(4,0).values())[-1], sg, list(self.sixteen_gong1(4,0).values())[:-1]
 
-    def gongs_discription1_text(self, sex):
+    def gongs_discription_text(self, sex):
         sixteengongs = self.sixteen_gong1(4,0)
         t = self.gen_life_gong_list(sex)[1]
         stars = self.gen_life_gong_list(sex)[2]
