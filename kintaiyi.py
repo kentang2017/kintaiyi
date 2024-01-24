f838769591896b8a1a0a85e27aefe4b9399565dc
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2024-01-24 09:12:41 +0800

    Update kintaiyi.py

--- kintaiyi.py
+++ kintaiyi.py
@@ -171,7 +171,9 @@ class Taiyi():
         alljq = jieqi_name
         j_q = jieqi.jq(self.year, self.month, self.day, self.hour, self.minute)
         dz = new_list(alljq, "冬至")[0:12]
+        dz_num = dict(zip(new_list(alljq, "冬至")[0:12],list(range(1,13))))
         hz = new_list(alljq, "夏至")[0:12]
+        hz_num = dict(zip(new_list(alljq, "夏至")[0:12],list(range(1,13))))
         jqmap = {tuple(config.new_list(alljq, "冬至")[0:12]):"冬至", tuple(config.new_list(alljq, "夏至")[0:12]):"夏至"}
         k = self.accnum(ji_style, taiyi_acumyear)%72
         if k == 0:
@@ -189,9 +191,9 @@ class Taiyi():
             #dundict = {"夏至":{tuple(list("辰巳午未申酉")):"陽遁", tuple(list("戌亥子丑寅卯")):"陰遁"}, "冬至":{tuple(list("辰巳午未申酉")):"陰遁", tuple(list("戌亥子丑寅卯")):"陽遁"}
             #dunk = multi_key_dict_get(dund, config.gangzhi(self.year, self.month, self.day, self.hour, self.minute)[3][1])
             if dun == "冬至":
-                if dz.index(j_q) % 2 == 0 and j_q != "冬至":
+                if dz_num.get(j_q) % 2 == 0 and j_q != "冬至":
                     a = multi_key_dict_get({tuple(list("戌亥子丑寅卯")):"陰遁", tuple(list("辰巳午未申酉")):"陽遁"}, config.gangzhi(self.year, self.month, self.day, self.hour, self.minute)[3][1])
-                if dz.index(j_q) % 2 != 0 or j_q == "冬至":
+                if dz_num.get(j_q) % 2 != 0 or j_q == "冬至":
                     a =multi_key_dict_get({tuple(list("辰巳午未申酉")):"陰遁", tuple(list("戌亥子丑寅卯")):"陽遁"}, config.gangzhi(self.year, self.month, self.day, self.hour, self.minute)[3][1])
                 #if test == 0
             if dun == "夏至":
