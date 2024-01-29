f572ca705758c45ab8773b0294b2efba73546f41
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2024-01-30 00:53:31 +0800

    Update kintaiyi.py

--- kintaiyi.py
+++ kintaiyi.py
@@ -187,7 +187,7 @@ class Taiyi():
             return {"文":f"{dun}{an2cn(k)}局", "數":k, "年":three_year, "積"+config.taiyi_name(ji_style)[0]+"數":self.accnum(ji_style, taiyi_acumyear) }
         if ji_style == 4:
             dun = multi_key_dict_get(jqmap, j_q)
-            #dund = dundict.get(dun)
+            dund = dundict.get(dun)
             #dundict = {"夏至":{tuple(list("辰巳午未申酉")):"陽遁", tuple(list("戌亥子丑寅卯")):"陰遁"}, "冬至":{tuple(list("辰巳午未申酉")):"陰遁", tuple(list("戌亥子丑寅卯")):"陽遁"}
             dunk = multi_key_dict_get(dund, config.gangzhi(self.year, self.month, self.day, self.hour, self.minute)[3][1])
             #if dun == "冬至":
