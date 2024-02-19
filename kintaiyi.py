345b6d2fcf27275e002a13d85604f97300c79e7b
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2024-02-19 22:16:37 +0800

    Update kintaiyi.py

--- kintaiyi.py
+++ kintaiyi.py
@@ -300,8 +300,8 @@ class Taiyi():
         s_f = self.sf_num(ji_style, taiyi_acumyear)
         sf = self.sf(ji_style, taiyi_acumyear)
         su_r = list(reversed(su))
-        c = dict(zip(new_list(sixteen, sf), new_list(su, s_f)))
-        suu = dict(zip(list(reversed(range(1,29))), su_r))
+        c = dict(zip(new_list(sixteen, sf), new_list(su_r, s_f)))
+        suu = dict(zip(range(1,29), su_r))
         snum = su.index(s_f)-sixteen.index(sf)+sixteen.index("巳") + 2 
         if sf == "坤":
            snum = su.index(s_f)-sixteen.index(sf)+sixteen.index("巳") - 2
