670022795367bda9a82cef552b4712c70e39fe2f
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2024-05-19 23:30:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -82,8 +82,6 @@ with st.sidebar:
     idate = st.text_input('輸入日期(如: 1997/8/8)', '')
     itime = st.text_input('輸入時間(如: 18:30)', '').replace("︰",":")
     #itime=st.time_input("時間",pdlm.now(tz='Asia/Shanghai').time())
-    default_value = datetime.now()
-    select_date = date_picker(picker_type=PickerType.time, value=default_value, key='date_picker')
     option = st.selectbox( '起盤方式', (' 年計太乙 ', ' 月計太乙 ', ' 日計太乙 ', ' 時計太乙 ', ' 分計太乙 ', ' 太乙命法 '))
     acum = st.selectbox( '太乙積年數', (' 太乙統宗 ', ' 太乙金鏡 ', ' 太乙淘金歌 ', ' 太乙局 '))
     sex_o = st.selectbox( '太乙命法性別', ('男', '女'))
@@ -96,6 +94,8 @@ with st.sidebar:
 
 with pan:
     output5 = st.empty()  
+    default_value = datetime.now()
+    select_date = date_picker(picker_type=PickerType.time, value=default_value, key='date_picker')
     with st_capture(output5.code):
         try:
             if manual:
