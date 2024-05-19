36fcdf2ff5ba8bd53dd52a92a3369240709f0f36
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2024-05-19 17:36:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -99,7 +99,6 @@ with st.sidebar:
             </style>
             """, unsafe_allow_html=True)
     col1, col2, col3 = st.columns([1,1,1])
-
     with col1:
        st.button('1')
     with col2:
