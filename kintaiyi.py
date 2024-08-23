15fa8592df7e14bac96509e092a0e6667811d4fe
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2024-08-23 11:30:12 +0800

    Update kintaiyi.py

--- kintaiyi.py
+++ kintaiyi.py
@@ -676,7 +676,7 @@ class Taiyi():
         if ji_style in [0,1]:
             return chart.gen_chart( list(self.sixteen_gong( ji_style, taiyi_acumyear).values())[-1], self.geteightdoors_text2(ji_style, taiyi_acumyear), list(self.sixteen_gong( ji_style, taiyi_acumyear).values())[:-1])
         if ji_style in [2]:
-            dict1 = self.gpan1(self.year, self.month, self.day, self.hour, self.minute)
+            dict1 = config.gpan1(self.year, self.month, self.day, self.hour, self.minute)
             middle = dict1[0]
             ng = dict1[1]
             return chart.gen_chart_day( list(self.sixteen_gong( ji_style, taiyi_acumyear).values())[-1] + middle, self.geteightdoors_text2(ji_style, taiyi_acumyear), ng, list(self.sixteen_gong( ji_style, taiyi_acumyear).values())[:-1], self.twenty_eightstar(ji_style, taiyi_acumyear))
