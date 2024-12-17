c9385d077cdb51be855239d458f2efc76e191017
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2024-12-17 11:07:55 +0800

    Update kintaiyi.py

--- kintaiyi.py
+++ kintaiyi.py
@@ -231,11 +231,11 @@ class Taiyi():
     def get_five_yuan_kook(self, ji_style, taiyi_acumyear):
         """太乙五子元局"""
         gz = config.gangzhi(self.year, self.month, self.day, self.hour, self.minute)
-        if ji_style != 4:
-            k = self.kook(ji_style, taiyi_acumyear).get("文")[0:2] + config.five_zi_yuan(self.kook(ji_style, taiyi_acumyear).get("數"), gz[ji_style])
-        if ji_style == 4:
-            k = self.kook(ji_style, taiyi_acumyear).get("文")[0:2] + config.min_five_zi_yuan(self.kook(ji_style, taiyi_acumyear).get("數"), gz[ji_style])
         try:
+            if ji_style != 4:
+                k = self.kook(ji_style, taiyi_acumyear).get("文")[0:2] + config.five_zi_yuan(self.kook(ji_style, taiyi_acumyear).get("數"), gz[ji_style])
+            if ji_style == 4:
+                k = self.kook(ji_style, taiyi_acumyear).get("文")[0:2] + config.min_five_zi_yuan(self.kook(ji_style, taiyi_acumyear).get("數"), gz[ji_style])
             return k
         except ValueError:
             return ""
