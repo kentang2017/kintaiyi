51516cc20dd94af1e64c3a3d45c8bee251f11da7
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2024-12-17 09:59:42 +0800

    Update kintaiyi.py

--- kintaiyi.py
+++ kintaiyi.py
@@ -7,7 +7,7 @@ import re
 import time
 import itertools
 from itertools import cycle
-import ephem
+from ephem import Date
 import numpy as np
 from cn2an import an2cn
 from taiyidict import tengan_shiji, su_dist
@@ -1303,7 +1303,8 @@ class Taiyi():
         return [(y + m + d + h + 55) % 64, config.gua.get((y + m + d + h + 55) % 64)]
 
     def year_gua(self):
-        num= self.life_start_gua()[0] + config.calculateAge(ephem.Date(self.year, self.month, self.day))
+        d = Date("{}/{}/{} {}:00:00.00".format(str(self.year).zfill(4), str(self.month).zfill(2), str(self.day).zfill(2), str(self.hour).zfill(2)))
+        num= self.life_start_gua()[0] + config.calculateAge(d)
         if num> 64:
             return [num, gua.get(num% 64)]
         else:
