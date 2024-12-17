7f0e91baafac8dc3a23773e20736732bb4caa44c
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2024-12-17 15:30:10 +0800

    Update kintaiyi.py

--- kintaiyi.py
+++ kintaiyi.py
@@ -712,7 +712,7 @@ class Taiyi():
         dict1 = self.taiyi_life(sex).get("十二命宮排列")
         res.update(dict1)
         sg = list(res.values())
-        return  list(self.sixteen_gong1(3,0).values())[-1], sg, list(self.sixteen_gong1(3,0).values())[:-1]
+        return  list(self.sixteen_gong1(4,0).values())[-1], sg, list(self.sixteen_gong1(4,0).values())[:-1]
 
     def convert_gongs_text(self, a, b):
         c = {}
