fff176876da164226845690825016ec962909256
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2025-05-31 20:08:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -90,17 +90,37 @@ def render_svg(svg, num):
     html(html_content, height=num)
 
 def render_svg1(svg, num):
-    """渲染靜態 SVG 圖表（無旋轉功能）"""
+    """渲染靜態 SVG 圖表（可點擊著色功能）"""
     # Validate SVG input
     if not svg or 'svg' not in svg.lower():
         st.error("Invalid SVG content provided")
         return
-    
     html_content = f"""
     <div style="margin: 0; padding: 0;">
       <svg id="static-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 {num} {num}" width="100%" height="auto" style="max-height: 400px; display: block; margin: 0 auto;">
         {svg}
       </svg>
+      <script>
+        const coloredSections = new Set();
+        const colors = ['#800080', '#FF69B4']; // Purple and Pink
+        let colorIndex = 0;
+
+        document.querySelectorAll('#static-svg g').forEach(group => {
+          group.addEventListener('click', () => {
+            const id = group.getAttribute('id') || 'default_' + Math.random().toString(36).substr(2, 9);
+            if (coloredSections.has(id)) {
+              // Remove color if already colored
+              group.removeAttribute('fill');
+              coloredSections.delete(id);
+            } else if (coloredSections.size < 2) {
+              // Add color if less than 2 sections are colored
+              group.setAttribute('fill', colors[colorIndex]);
+              coloredSections.add(id);
+              colorIndex = (colorIndex + 1) % colors.length;
+            }
+          });
+        });
+      </script>
     </div>
     <style>
         #static-svg {{
@@ -114,27 +134,6 @@ def render_svg1(svg, num):
     """
     html(html_content, height=num)
 
-def timeline(data, height=800):
-    """渲染時間線組件"""
-    if isinstance(data, str):
-        data = json.loads(data)
-    json_text = json.dumps(data)
-    source_param = 'timeline_json'
-    source_block = f'var {source_param} = {json_text};'
-    cdn_path = 'https://cdn.knightlab.com/libs/timeline3/latest'
-    css_block = f'<link title="timeline-styles" rel="stylesheet" href="{cdn_path}/css/timeline.css">'
-    js_block = f'<script src="{cdn_path}/js/timeline.js"></script>'
-    htmlcode = f'''
-        {css_block}
-        {js_block}
-        <div id='timeline-embed' style="width: 95%; height: {height}px; margin: 1px;"></div>
-        <script type="text/javascript">
-            var additionalOptions = {{ start_at_end: false, is_embed: true, default_bg_color: {{r:14, g:17, b:23}} }};
-            {source_block}
-            timeline = new TL.Timeline('timeline-embed', {source_param}, additionalOptions);
-        </script>
-    '''
-    components.html(htmlcode, height=height)
 
 @contextmanager
 def st_capture(output_func):
@@ -176,7 +175,7 @@ with st.sidebar:
     acum = st.selectbox('太乙積年數', ('太乙統宗', '太乙金鏡', '太乙淘金歌', '太乙局'))
     ten_ching = st.selectbox('太乙十精', ('無', '有'))
     sex_o = st.selectbox('太乙命法性別', ('男', '女'))
-    rotation = st.selectbox('轉盤', ('固定', '轉動'))
+    rotation = st.selectbox('轉盤', ('固定可着色', '轉動'))
     
     num_dict = {'時計太乙': 4, '年計太乙': 0, '月計太乙': 1, '日計太乙': 2, '分計太乙': 3, '太乙命法': 5}
     style = num_dict[option]
