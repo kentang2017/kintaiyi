91c8366a97861bfeb3d08d3d1afde5e4717292d2
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2026-08-06 00:19:03 +0800

    Update kintaiyi.py

--- src/kintaiyi/kintaiyi.py
+++ src/kintaiyi/kintaiyi.py
@@ -184,7 +184,7 @@ _XIU_TO_BRANCH = {
     "翼": "巳", "軫": "巳",
 }
 _XIU_SEQ_LIST = list("角亢氐房心尾箕斗牛女虛危室壁奎婁胃昴畢觜參井鬼柳星張翼軫")
-_XIU_OFFSET = 200.0  # 與 _compute_rotate_28 一致
+_XIU_OFFSET = 120.0  # 與 _compute_rotate_28 一致
 
 
 def _lon_to_xiu_name(lon, offset=None, degrees=None):
